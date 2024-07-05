979ddb68a81b762016b1adbdb7c8447730c9b3bd
CYBER HACK HM <156664522+HsnCyber@users.noreply.github.com>
2024-07-05 12:10:09 +0500

    Update config.js

--- config.js
+++ config.js
@@ -32,8 +32,8 @@ global.waPresence = process.env.WAPRESENCE || "online";
 global.readcmds = process.env.READ_COMMAND || "false";
 global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
-global.read_status = process.env.AUTO_READ_STATUS || "true";
-global.save_status = process.env.AUTO_SAVE_STATUS || "false";
+global.read_status = process.env.AUTO_READ_STATUS || "false";
+global.save_status = process.env.AUTO_SAVE_STATUS || "true";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "";
 global.read_status_from = process.env.READ_STATUS_FROM || "";
 
@@ -45,15 +45,15 @@ global.SESSION_ID =
   "Byte;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiR1ArVUhvZnp1L1A3SkgwNzFRS3JkQk1IWVRPZ1ZHZHRQNThKdGVXaHFYYz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSTN5dTR3RUVBL1kzTDRTT0wvM2JLSXFqN05Mc0hyT0ZlcDFlRU41RDJRWT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJjQ2ZqTHlIb2hMc0Zrd1pjSE5iUEFmYnNFYXJ5ZWxweFlPaGkwaG9RRldFPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI2V2s1QTR0Y1pJQXZWbkNjOW5JUTRJM1RUa0NOR2JBVWZxZTEweUNSMUEwPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ik1KVnR4eFozQkFsaWdZeUhrbzFRamZ2MVhQRkNtZTFnRW1HaDZUeURPM3c9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlFsK1A3dEpEdlBrL0Jib1laUWtmaHVVYWxWdnRSdTQzVUlmMm5BOGtsMVE9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNkd2T0tHQlhRWnZZbUVaaWRXTW95cjZzK3R0aDMydWZGbFZPdXVlMTNtTT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiUlNhTlk2eURYdk55MDN1VnJoTXFSUGtOb1l5T2lwd0d0cEUrTWYyS3BYYz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ik1ndW05cEJZZWFDTEdqUlRjeVQ4N2o2MGRsWVd6UXQ3WFRMUWk0L2o1NlUwNnh3VURuS3hwNGo2MEJFUE5kV2pGTlpBVmVIS1JwVUxtRVJqWGQ4K2hRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTksImFkdlNlY3JldEtleSI6IkN1eGd0c3cxdTZVYUl4bllLN0ZSeVMrV3NGSEdCQ2I0a29ydy9BSlVrTFk9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMiwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMyLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6ImJXeDBBYzBrUU1hYmJDaXhyeFlMLVEiLCJwaG9uZUlkIjoiMzgxZDBkZDAtMzBkMi00YzUzLTg4ZTItZWJlOTI3MGY3NWIzIiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InNSdnEwb2VKb0dwSWVrZEtvMlozSjJVWHIzYz0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIwZGcwUlp5ckVUMDJ2c1RjOUFtdmtTVCtzclU9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiWE5XNVpUNjQiLCJtZSI6eyJpZCI6IjkyMzE4MzgzMDc2Njo2QHMud2hhdHNhcHAubmV0In0sImFjY291bnQiOnsiZGV0YWlscyI6IkNOTDExN2dCRUxTeW5yUUdHQUVnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJKbzU3N2VLenNxanZCZ0V6Wnk4cXhmSWt3Z0FHejRoemlvTUVyMXlWWHg0PSIsImFjY291bnRTaWduYXR1cmUiOiJhU2JnbVl3d0dyY3RtNS8wZVBwanpwaWplcER5TkpqM1dIdlNUaXNmdmJIZ1lwZzJPMGlPYmhKRmsrckg3bG10Vk1XY1d0VTZ5MFBQUGRHdWgrWWdBUT09IiwiZGV2aWNlU2lnbmF0dXJlIjoiMEc2bWVNYU9sNkU3Z01GRGN6Nnd0VEtZZzZOV3p2anBJWW9PMUZ0UFdYU0w4SGJUMnJEVjUzbmMyWEh6NHBHZHJyTG9oQ2tDUkVjZ040VVpXY1dOaHc9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI5MjMxODM4MzA3NjY6NkBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJTYU9lKzNpczdLbzd3WUJNMmN2S3NYeUpNSUFCcytJYzRxREJLOWNsVjhlIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzIwMTYyNjE0fQ=="
 module.exports = {
   menu: process.env.MENU || "",
-  HANDLERS: process.env.PREFIX || ",",
+  HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "*Powered By TalkDrove*",
+  caption: process.env.CAPTION || "*Powered By prince*",
   author: process.env.PACK_AUTHER || "TalkDrove",
   packname: process.env.PACK_NAME || "♥️",
   botname: process.env.BOT_NAME || "BYTE-MD",
-  ownername: process.env.OWNER_NAME || "Hamza",
-  errorChat: process.env.ERROR_CHAT || "923072380380",
+  ownername: process.env.OWNER_NAME || "prince",
+  errorChat: process.env.ERROR_CHAT || "923183830766",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
